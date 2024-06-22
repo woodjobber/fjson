268c53bb369cc190fb1e4a93c9aaef8d7d5b6c94
soer <474436731@qq.com>
2024-06-22 23:00:16 +0800

    [INFO]: 自动构建并发布

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "43eb3ef1a5d1f985ecd09b2ce30dfcaf",
-"index.html": "9e52f2da783f34f344e2f65c30fcc115",
-"/": "9e52f2da783f34f344e2f65c30fcc115",
-"main.dart.js": "7dfb3aeceb035c22304f297184ca9cb7",
+"index.html": "3fc6702ef3c46bb61fffdf605cbb4859",
+"/": "3fc6702ef3c46bb61fffdf605cbb4859",
+"main.dart.js": "30b305cef1d817f592f6a7999c48de2b",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "icons/favicon-16x16.png": "4a571da03c7a7d6ebbdb2665bbfcde99",
 "icons/favicon.ico": "31f4cb5e591777358def3321c65feee0",
